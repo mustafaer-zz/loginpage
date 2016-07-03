199e899f5b0603d86ff86483067a9f71ac075439
Mustafa ER <mustafaer243@gmail.com>
2016-07-03 05:34:32 +0300

    Add files via upload

---
+++ loginpage/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="css/login.css" rel="stylesheet" type="text/css"/>
+    <title></title>
+</head>
+<body>
+    <div>
+        <div class="hdr">
+            <div class="mg"><div style="margin-left: 65px;"><b><label style="font-size: 32px; ">SAUEEM</label></b>
+            </div>
+            </div>
+            <div class="nv">
+                <ul>
+                    <li> <a href="#">Home</a></li>
+                    <li> <a href="#">Project</a></li>
+                    <li> <a href="#">About Us</a></li>
+                    <li> <a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="homepage-inf"> 
+                <p class="txt-tt">WELCOME TO SAUEEM</p>
+                <br>
+                <p class="txt-con">This page designed for SAUEEM by Mustafa ER. This is a Open Source Login Page project.</p>
+            </div>
+            <div class="homepage-sign">
+                <div class="con">
+                    <p style="padding-top: 45px;"><input class="inp" name="username" type="text" size="30" placeholder="User Name"/>
+                    <br>
+                    <br>
+                    <input class="inp" name="passwd" type="password" size="30" placeholder="Password"/>
+                    <br>
+                    <br>
+                    <b><input class="btn-login" name="login-btn" type="submit" value="Sign in to SAUEEM"></input></b></p>
+        <label style="font-size: 12px; opacity: 0.5;">SAUEEM Project © by <a style="color: #fff; opacity: 1;" href="http://mustafaerblog.wordpress.com" target="_blank">Mustafa ER</a></label>
+            </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
